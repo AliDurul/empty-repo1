9d5873df4ae87100a1a63b217511fdff4a2bd95c
Ali <alidrl26@gmail.com>
2023-10-01 16:07:03 +0200

    hmr bug solved

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,48 +1,43 @@
-import { useEffect } from "react";
-import Message from "./Message"
+import Message from "./Message";
 import { collection, query, onSnapshot, orderBy, limit } from "firebase/firestore";
+import { useEffect, useRef, useState } from "react";
 import { db } from "../auth/firebase";
-import { useState } from "react";
-import { useRef } from "react";
 
 const ChatBox = () => {
-  const messagesEndRef = useRef()
-  const [messages, setMessages] = useState([])
+  const messagesEndRef = useRef();
+  const [messages, setMassages] = useState([]);
 
   const scrollToBottom = () => {
-    messagesEndRef.current.scrollIntoView({behavior:'smooth'})
-  }
+    messagesEndRef.current.scrollIntoView({ behavior: "smooth"})
+  };
 
   useEffect(scrollToBottom, [messages])
-  
 
   useEffect(() => {
     const q = query(
       collection(db, "messages"),
-      orderBy('createdAt'), 
-      limit(50)
+      orderBy("createdAt"),
+      limit(50),
     );
-
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       const messages = [];
       querySnapshot.forEach((doc) => {
         messages.push({ ...doc.data(), id: doc.id });
       });
-      setMessages(messages);
+      setMassages(messages);
     });
 
-    return () => unsubscribe
-  }, [])
-
+    return () => unsubscribe;
+  }, []);
 
   return (
-    <div className="pb-44 pt-20 containerWrap px-2">
-      {
-        messages.map(message => <Message message={message} key={message.id} />)
-      }
+    <div className="pb-44 pt-20 containerWrap">
+      {messages.map((message) => (
+        <Message key={message.id} message={message} />
+      ))}
       <div ref={messagesEndRef}></div>
     </div>
-  )
-}
+  );
+};
 
-export default ChatBox
+export default ChatBox;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Hello there 👋🏻</h1>
+          <h1 className="text-5xl font-bold">Hi Everybody Welcome to Lee Chat👋🏻</h1>
           <p className="py-6"> Join the conversation, meet new people, and make connections in one shared room.</p>
           <button className="btn btn-neutral dark:btn-primary" onClick={hadnleLogin}>LOGIN WITH GOOGLE</button>
         </div>
