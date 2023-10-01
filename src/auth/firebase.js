8a5102a4065e070ca18a389450fab24cb192bcbd
Ali <alidrl26@gmail.com>
2023-10-01 21:32:34 +0200

    .env file added

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCbKuHywroppQO-lEesQX8OsE6JmZFFXho",
-  authDomain: "chat-app-ff4d4.firebaseapp.com",
-  projectId: "chat-app-ff4d4",
-  storageBucket: "chat-app-ff4d4.appspot.com",
-  messagingSenderId: "545439327202",
-  appId: "1:545439327202:web:852bd7de421f0212a20fbb",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
