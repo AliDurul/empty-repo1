3e63b3a30366e8029885b1608ceb05c64ed42c21
AliDurul <alidrl26@gmail.com>
2024-10-28 17:01:46 +0200

    Refactor ESLint configuration to disable react/prop-types rule

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,45 @@
-import { Routes } from "react-router-dom"
-import Navbar from "./components/Navbar"
-import ChatRoom from "./pages/ChatRoom"
-import { Route } from "react-router-dom"
-import Login from "./pages/Login"
-import { PrivateRoute } from "./routes/PrivateRoute"
-import { AuthProvider } from "./context/AuthContext"
+import { Nav } from "./components";
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from "./sections";
 
-function App() {
-
-
+const App = () => {
   return (
-    <>
-      <AuthProvider>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/chat" element={<PrivateRoute />}>
-            <Route path="" element={<ChatRoom />} />
-          </Route>
-        </Routes>
-      </AuthProvider>
-    </>
-  )
-}
+    <main className='relative'>
+      <Nav />
+      <section className='xl:padding-l wide:padding-r padding-b'>
+        <Hero />
+      </section>
+      <section className='padding'>
+        <PopularProducts />
+      </section>
+      <section className='padding'>
+        <SuperQuality />
+      </section>
+      <section className='padding-x py-10'>
+        <Services />
+      </section>
+      <section className='padding'>
+        <SpecialOffer />
+      </section>
+      <section className='bg-pale-blue padding'>
+        <CustomerReviews />
+      </section>
+      <section className='padding-x sm:py-32 py-16 w-full'>
+        <Subscribe />
+      </section>
+      <section className=' bg-black padding-x padding-t pb-8'>
+        <Footer />
+      </section>
+    </main>
+  );
+};
 
-export default App
+export default App;
